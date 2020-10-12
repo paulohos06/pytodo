b080ddc53707578f49179e4bb0eac7f0e0c2fd86
Paulo Henrique <paulo.henrique06@hotmail.com>
2020-10-12 18:20:48 -0300

    upgrade activity model admin

--- pytodo/core/admin.py
+++ pytodo/core/admin.py
@@ -5,16 +5,17 @@ from pytodo.core.models import Activity
 
 # Register your models here.
 class ActivityAdmin(admin.ModelAdmin):
-    list_display = [
-        "system_name",
-        "num_act",
-        "date_act",
-        "status",
-        "created_at",
-    ]
+    list_display = ["system_name", "num_act", "status", "date_act"]
     date_hierarchy = "date_act"
     search_fields = ("system_name", "num_act", "date_act", "status")
     list_filter = ("system_name", "date_act", "status")
 
+
+SITE_NAME = "Gestão de Atividades"
+
+admin.site.header = SITE_NAME
+admin.site.site_header = SITE_NAME
+# admin.site.index_title = ''
+admin.site.site_title = SITE_NAME
 
 admin.site.register(Activity, ActivityAdmin)
--- pytodo/core/models.py
+++ pytodo/core/models.py
@@ -9,14 +9,29 @@ class Activity(models.Model):
         ("R", "Rejeitada"),
     ]
 
-    num_act = models.CharField("demanda", max_length=15)
-    system_name = models.CharField("sistema", max_length=10)
-    description = models.TextField("descrição", max_length=100)
+    num_act = models.CharField(
+        "demanda",
+        max_length=30,
+        blank=True,
+        help_text="Identificação da atividade.",
+    )
+    system_name = models.CharField(
+        "sistema", max_length=10, blank=True, help_text="Sigla do sistema."
+    )
+    description = models.TextField(
+        "descrição",
+        max_length=100,
+        help_text="Descrição sobre o que deve ser feito na atividade.",
+    )
     date_act = models.DateTimeField(
         "data", help_text="A data em que a atividade foi realizada."
     )
     status = models.CharField(
-        "status", max_length=1, blank=True, choices=STATUS_CHOICES
+        "status",
+        max_length=1,
+        blank=True,
+        choices=STATUS_CHOICES,
+        help_text="Flag para indicar qual a situação da atividade.",
     )
     comment = models.TextField(
         "comentário",
@@ -26,10 +41,10 @@ class Activity(models.Model):
     )
     created_at = models.DateTimeField("criado em", auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.system_name} - {self.num_act}"
+
     class Meta:
         verbose_name = "Atividade"
         verbose_name_plural = "Atividades"
         ordering = ("-date_act",)
-
-        def __str__(self):
-            return f"{self.system_name} - {self.num_act}"
